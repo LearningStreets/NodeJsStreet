4769739595657092691ed1a7bcf5c46a7764e50b
Atul Yadav <atulyadav5566@live.com>
2021-05-19 21:37:24 +0530

    change code as per basic application

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-var createError = require('http-errors');
+// var msg = "Node: Hello World Program"
+// console.log(msg);
+
+
+// importing express package such that we can utilize it's features
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// Creating a instance for express
 var app = express();
+var port = 3005; // declaring the port such that we can run the application on this 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// Basic Get method which will return the message. 
+app.get('/', function(req, res) {
+  res.send('Node: Hello World Program');
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// declaring below listen method which will listen on the assigned port number
+app.listen(port, function() {
+  console.log('Logging this - Node: Hello World Program')
+})
